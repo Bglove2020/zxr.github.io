8043c7679836a06ea96783ec6a80606e3a1337c5
zxr <2422335861@qq.com>
2024-03-20 12:37:37 +0800

    重构登录界面CSS结构，更换主题颜色

--- log_in.js
+++ log_in.js
@@ -1,15 +1,18 @@
 let password_input =document.querySelector("#pass");
 let password_tu =document.querySelector("#pass_tu");
-password_tu.zhuangtai =1;
 password_tu.onclick =function (){
-    if(password_tu.zhuangtai==1){
+    if(password_tu.dataset.flag ==0){
+        console.log(password_tu.dataset.flag)
+        console.log(typeof password_tu.dataset.flag)
         password_tu.src="resource/eye_open.svg";
         password_input.type="text";
-        password_tu.zhuangtai=0;
+        password_tu.dataset.flag=1;
     }
     else{
+        console.log(password_tu.dataset.flag)
+        console.log(typeof password_tu.dataset.flag)
         password_tu.src="resource/eye_close.svg";
         password_input.type="password";
-        password_tu.zhuangtai=1;
+        password_tu.dataset.flag=0;
     }
 }
